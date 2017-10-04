69640e41ccb43d6b8efc028f745fa005144217da
Lei Ni <nilei97@163.com>
2017-10-05 00:11:09 +0800

    Minor cleanup

--- ec2_settings.py
+++ ec2_settings.py
@@ -1,13 +1,14 @@
 # what regions to use
-regions = ['us-west-1', 'us-west-2', 'us-east-1']
+regions = ['us-west-1', 'us-west-2', 'us-east-1', 'ap-southeast-2']
 
 # Ubuntu 16.04LTS ebs paravirtual
 ami_name = {
   "us-west-2": "ami-20c04f40",
   "us-east-1": "ami-2657f630",
-  "us-west-1": "ami-a66c35c6",
+  "us-west-1": "ami-2d5c6d4d",
   "eu-west-1": "ami-405f7226",
-  "ap-northeast-1": "ami-1bfdb67c",
+  "ap-northeast-1": "ami-8422ebe2",
+  'ap-southeast-2': "ami-391ff95b",
 }
 
 security_group_name = "raft-sg"
--- local_proxy.py
+++ local_proxy.py
@@ -1,6 +1,6 @@
 import os
 
 # thank you GFC!
-os.environ["HTTP_PROXY"] = "http://10.0.0.5:8118"
-os.environ["HTTPS_PROXY"] = "https://10.0.0.5:8118"
+os.environ["HTTP_PROXY"] = "http://localhost:8080"
+os.environ["HTTPS_PROXY"] = "https://localhost:8080"
 
